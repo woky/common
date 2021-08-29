f5dcffe1d0ef97295730326e6e1d92118c1e04db
Reinhard Tartler <siretart@tauware.de>
2021-08-29 08:33:45 +0200

    0.42.1+ds1-1 (patches unapplied)
    Imported using git-ubuntu import.

--- libimage/pull_test.go
+++ libimage/pull_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	goruntime "runtime"
 	"testing"
 
 	"github.com/containers/common/pkg/config"
@@ -17,6 +18,12 @@ func TestPull(t *testing.T) {
 	ctx := context.Background()
 	pullOptions := &PullOptions{}
 	pullOptions.Writer = os.Stdout
+
+	// Make sure that parsing errors of the daemon transport are returned
+	// and that we do not fallthrough attempting to pull the specified
+	// string as an image from a registry.
+	_, err := runtime.Pull(ctx, "docker-daemon:alpine", config.PullPolicyAlways, pullOptions)
+	require.Error(t, err, "return parsing error from daemon transport")
 
 	for _, test := range []struct {
 		input       string
@@ -42,6 +49,8 @@ func TestPull(t *testing.T) {
 		{"docker://alpine", false, 1, []string{"docker.io/library/alpine:latest"}},
 		{"docker.io/library/alpine", false, 1, []string{"docker.io/library/alpine:latest"}},
 		{"docker://docker.io/library/alpine", false, 1, []string{"docker.io/library/alpine:latest"}},
+		{"quay.io/libpod/alpine@sha256:634a8f35b5f16dcf4aaa0822adc0b1964bb786fca12f6831de8ddc45e5986a00", false, 1, []string{"quay.io/libpod/alpine@sha256:634a8f35b5f16dcf4aaa0822adc0b1964bb786fca12f6831de8ddc45e5986a00"}},
+		{"quay.io/libpod/alpine:pleaseignorethistag@sha256:634a8f35b5f16dcf4aaa0822adc0b1964bb786fca12f6831de8ddc45e5986a00", false, 1, []string{"quay.io/libpod/alpine@sha256:634a8f35b5f16dcf4aaa0822adc0b1964bb786fca12f6831de8ddc45e5986a00"}},
 	} {
 		pulledImages, err := runtime.Pull(ctx, test.input, config.PullPolicyAlways, pullOptions)
 		if test.expectError {
@@ -64,4 +73,47 @@ func TestPull(t *testing.T) {
 		assert.Equal(t, image.ID(), rmReports[0].ID)
 		assert.True(t, rmReports[0].Removed)
 	}
+}
+
+func TestPullPlatforms(t *testing.T) {
+	runtime, cleanup := testNewRuntime(t)
+	defer cleanup()
+	ctx := context.Background()
+	pullOptions := &PullOptions{}
+	pullOptions.Writer = os.Stdout
+
+	localArch := goruntime.GOARCH
+	localOS := goruntime.GOOS
+
+	pulledImages, err := runtime.Pull(ctx, "busybox", config.PullPolicyAlways, pullOptions)
+	require.NoError(t, err, "pull busybox")
+	require.Len(t, pulledImages, 1)
+
+	image, _, err := runtime.LookupImage("busybox", nil)
+	require.NoError(t, err, "lookup busybox")
+	require.NotNil(t, image, "lookup busybox")
+
+	image, _, err = runtime.LookupImage("busybox", &LookupImageOptions{Architecture: localArch})
+	require.NoError(t, err, "lookup busybox - by local arch")
+	require.NotNil(t, image, "lookup busybox - by local arch")
+
+	image, _, err = runtime.LookupImage("busybox", &LookupImageOptions{OS: localOS})
+	require.NoError(t, err, "lookup busybox - by local arch")
+	require.NotNil(t, image, "lookup busybox - by local arch")
+
+	_, _, err = runtime.LookupImage("busybox", &LookupImageOptions{Architecture: "bogus"})
+	require.Error(t, err, "lookup busybox - bogus arch")
+
+	_, _, err = runtime.LookupImage("busybox", &LookupImageOptions{OS: "bogus"})
+	require.Error(t, err, "lookup busybox - bogus OS")
+
+	pullOptions.Architecture = "arm"
+	pulledImages, err = runtime.Pull(ctx, "busybox", config.PullPolicyAlways, pullOptions)
+	require.NoError(t, err, "pull busybox - arm")
+	require.Len(t, pulledImages, 1)
+	pullOptions.Architecture = ""
+
+	image, _, err = runtime.LookupImage("busybox", &LookupImageOptions{Architecture: "arm"})
+	require.NoError(t, err, "lookup busybox - by arm")
+	require.NotNil(t, image, "lookup busybox - by local arch")
 }
--- pkg/auth/cli.go
+++ pkg/auth/cli.go
@@ -14,12 +14,14 @@ type LoginOptions struct {
 	// CLI flags managed by the FlagSet returned by GetLoginFlags
 	// Callers that use GetLoginFlags should not need to touch these values at all; callers that use
 	// other CLI frameworks should set them based on user input.
-	AuthFile      string
-	CertDir       string
-	Password      string
-	Username      string
-	StdinPassword bool
-	GetLoginSet   bool
+	AuthFile           string
+	CertDir            string
+	Password           string
+	Username           string
+	StdinPassword      bool
+	GetLoginSet        bool
+	Verbose            bool // set to true for verbose output
+	AcceptRepositories bool // set to true to allow namespaces or repositories rather than just registries
 	// Options caller can set
 	Stdin                     io.Reader // set to os.Stdin
 	Stdout                    io.Writer // set to os.Stdout
@@ -31,8 +33,9 @@ type LogoutOptions struct {
 	// CLI flags managed by the FlagSet returned by GetLogoutFlags
 	// Callers that use GetLogoutFlags should not need to touch these values at all; callers that use
 	// other CLI frameworks should set them based on user input.
-	AuthFile string
-	All      bool
+	AuthFile           string
+	All                bool
+	AcceptRepositories bool // set to true to allow namespaces or repositories rather than just registries
 	// Options caller can set
 	Stdout                    io.Writer // set to os.Stdout
 	AcceptUnspecifiedRegistry bool      // set to true if allows logout with unspecified registry
@@ -47,6 +50,7 @@ func GetLoginFlags(flags *LoginOptions) *pflag.FlagSet {
 	fs.StringVarP(&flags.Username, "username", "u", "", "Username for registry")
 	fs.BoolVar(&flags.StdinPassword, "password-stdin", false, "Take the password from stdin")
 	fs.BoolVar(&flags.GetLoginSet, "get-login", false, "Return the current login user for the registry")
+	fs.BoolVarP(&flags.Verbose, "verbose", "v", false, "Write more detailed information to stdout")
 	return &fs
 }
 
